21ffffb8f9530df9112d9fb0313a5d89a87855e4
cchhookkee <161779873+chokedesigns@users.noreply.github.com>
2026-01-19 18:04:37 -0600

    staging test

--- shared/drop-params/drop-params.js
+++ shared/drop-params/drop-params.js
@@ -19,7 +19,7 @@ export default {
   // ============================================================
   dropDate: {
     month: "January",    // "January"–"December", "Jan"–"Dec", or "1"–"12"
-    day:   "28",          // "1"–"31"
+    day:   "27",          // "1"–"31"
     year:  "2026"         // four-digit year, "XXXX"
   },
 
