1d19304635b266d08d856e54ec71fb0e03fb0eb4
cchhookkee <161779873+chokedesigns@users.noreply.github.com>
2026-01-27 17:58:18 -0600

    drop params published to git checklist

--- shared/drop-params/drop-params.js
+++ shared/drop-params/drop-params.js
@@ -19,7 +19,7 @@ export default {
   // ============================================================
   dropDate: {
     month: "January",    // "January"–"December", "Jan"–"Dec", or "1"–"12"
-    day:   "30",          // "1"–"31"
+    day:   "29",          // "1"–"31"
     year:  "2026"         // four-digit year, "XXXX"
   },
 
